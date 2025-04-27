404029486b952d0938ac1e7f713c84a17ffd0cd7
jasonmayyo <jason.m1122332@gmail.com>
2025-04-27 19:38:20 +0200

    Initial Commit

---
+++ OneThing/OneThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneThingApp.swift
+//  OneThing
+//
+//  Created by Jason Mayo on 2025/04/27.
+//
+
+import SwiftUI
+
+@main
+struct OneThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
